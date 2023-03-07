6749ed7047304f5142916d417514117f8fdaca9b
r180683 <118116144+r180683@users.noreply.github.com>
2023-03-07 19:40:19 +0530

    Update index.js

--- src/components/VaccinationByGender/index.js
+++ src/components/VaccinationByGender/index.js
@@ -7,30 +7,29 @@ const VaccinationByGender = props => {
   return (
     <div className="vacc-by-gender-container">
       <h1 className="vac-coverage">Vaccination by gender</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="50%"
-            cy="60%"
-            data={vaccinationByGender}
-            startAngle={0}
-            endAngle={180}
-            innerRadius="40%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="Male" fill="#fecba6" />
-            <Cell name="Female" fill="#b3d23f" />
-            <Cell name="Others" fill="#a44c9e" />
-          </Pie>
-          <Legend
-            iconType="circle"
-            layout="horizontal"
-            verticalAlign="bottom"
-            align="center"
-          />
-        </PieChart>
-      </ResponsiveContainer>
+
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="50%"
+          cy="60%"
+          data={vaccinationByGender}
+          startAngle={0}
+          endAngle={180}
+          innerRadius="40%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="Male" fill="#fecba6" />
+          <Cell name="Female" fill="#b3d23f" />
+          <Cell name="Others" fill="#a44c9e" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+        />
+      </PieChart>
     </div>
   )
 }
